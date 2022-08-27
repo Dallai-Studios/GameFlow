4e3d56ed95c7df11c23423fdec5f2dc623a98f90
Renan Souza <renan.souza@powerpush.com.br>
2022-08-27 18:37:49 -0300

    feat(Game general Config) DS-1 added new tool tips on the scriptable object

--- Assets/GameFlow/General/Scriptable Objects/GameGeneralConfiguration.cs
+++ Assets/GameFlow/General/Scriptable Objects/GameGeneralConfiguration.cs
@@ -5,10 +5,20 @@ namespace GameFlow.General.ScriptableObjects
     [CreateAssetMenu(fileName = "Game General Config", menuName = "Game Flow/General/Config/Game General Config")]
     public class GameGeneralConfiguration : ScriptableObject
     {
-        public float MasterVolume;
-        public float MusicVolume;
-        public float SoundEffectsVolume;
-        public float VoiceVolume;
+        [Header("Game General Configurations")]
+        [Tooltip("Define the game master volume")]
+        [Range(0,1)] public float MasterVolume;
+
+        [Tooltip("Define the game music volume")]
+        [Range(0,1)] public float MusicVolume;
+
+        [Tooltip("Define the game sound effects volume")]
+        [Range(0,1)] public float SoundEffectsVolume;
+
+        [Tooltip("Define the game voice volume in case of dialogs")]
+        [Range(0,1)] public float VoiceVolume;
+
+        [Tooltip("Define the game target frame rate")]
         public int TargetFrameRate;
     }
 }
