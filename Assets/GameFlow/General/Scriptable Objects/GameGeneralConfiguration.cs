8790ca4b93c8cd74d0d4e0e5e1780233724e5cf1
Renan Souza <renan.souza@powerpush.com.br>
2022-08-27 18:59:30 -0300

    feat(Game general Config) DS-1 Add events Now we can raise events when the values are changed

--- Assets/GameFlow/General/Scriptable Objects/GameGeneralConfiguration.cs
+++ Assets/GameFlow/General/Scriptable Objects/GameGeneralConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace GameFlow.General.ScriptableObjects
@@ -8,17 +9,28 @@ namespace GameFlow.General.ScriptableObjects
         [Header("Game General Configurations")]
         [Tooltip("Define the game master volume")]
         [Range(0,1)] public float MasterVolume;
+        public event Action OnChangeMasterVolume;
 
         [Tooltip("Define the game music volume")]
         [Range(0,1)] public float MusicVolume;
+        public event Action OnChangeMusicVolume;
 
         [Tooltip("Define the game sound effects volume")]
         [Range(0,1)] public float SoundEffectsVolume;
+        public event Action OnChangeSoundEffectsVolume;
 
         [Tooltip("Define the game voice volume in case of dialogs")]
         [Range(0,1)] public float VoiceVolume;
+        public event Action OnChangeVoiceVolume;
 
         [Tooltip("Define the game target frame rate")]
         public int TargetFrameRate;
+        public event Action OnChangeTargetFrameRate;
+
+        public void RaiseEventOnChangeMasterVolume() => this.OnChangeMasterVolume?.Invoke();
+        public void RaiseEventOnChangeMusicVolume() => this.OnChangeMusicVolume?.Invoke();
+        public void RaiseEventOnChangeSoundEffectsVolume() => this.OnChangeSoundEffectsVolume?.Invoke();
+        public void RaiseEventOnChangeVoiceVolume() => this.OnChangeVoiceVolume?.Invoke();
+        public void RaiseEventOnChangeTargetFrameRate() => this.OnChangeTargetFrameRate?.Invoke();
     }
 }
