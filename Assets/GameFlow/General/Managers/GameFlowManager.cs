b764574b597f1eaa192662a1e6a14b1da80ea6b3
Renan Souza <renan.souza@powerpush.com.br>
2022-09-10 16:37:06 -0300

    on development (#8)
    * on development
    
    * more fixes
    
    * more stuff
    
    * more fixes
    
    * more fixes
    
    * more fixes
    
    * more fixes

--- Assets/GameFlow/General/Managers/GameFlowManager.cs
+++ Assets/GameFlow/General/Managers/GameFlowManager.cs
@@ -5,21 +5,15 @@ namespace GameFlow.General.Managers
 {
     public class GameFlowManager : MonoBehaviour
     {
-        #region "Public Properties"
-            public static GameFlowManager Instance;
-        #endregion
+        public static GameFlowManager Instance;
 
-        #region "Serialized Properties"
-            [Header("Game Default Parameters")]
+        [Header("Game Default Parameters")]
             [Tooltip("Define the Game general configuration parameters")]
             [SerializeField] private GameGeneralConfiguration _gameGeneralConfig;
-        #endregion
 
-        #region "Monobehaviour Methods"
-            private void Start()
+        private void Start()
             {
                 if(GameFlowManager.Instance is null) GameFlowManager.Instance = this;
             }
-        #endregion
     }
 }
